fad7541201891d29693cad72d7e478f86c15e41f
Zain-Ashfaq <zashfaq00@gmail.com>
2023-06-14 11:26:27 +0100

    Inlcuded age and temp tasks

---
+++ if_and_elif.py
@@ -0,0 +1,34 @@
+# Control flow
+
+# Like giving Python a recipe or an order to do things
+
+# Conditional statements
+
+age = int(input("Enter your age:\n"))
+
+if age >= 18:
+    print("you can watch all movies")
+elif age >= 15:
+    print("you can watch 15 and less rated movies")
+elif age >= 12:
+    print("you can watch 12 and less rated movies")
+elif age >= 8:
+    print("you can watch 8 and less rated movies")
+else:
+    print("you can only watch U rated ")
+
+# user temp
+temp = int(input("Enter the temperature:\n"))
+if temp >= 35:
+    print("boiling")
+elif temp >= 30:
+    print("too hot")
+elif temp >= 25:
+    print("nice weather")
+elif temp >= 15:
+    print("standard")
+else:
+    print("probably raining")
+
+#
+
